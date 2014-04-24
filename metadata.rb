b38df66738c3e2750a356a29d4d4cf90242021a6
Shane Sveller <shanesveller@gmail.com>
2014-04-24 17:41:56 -0500

    Start to flesh out metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'apt'
+
+supports 'ubuntu', '= 12.04'
+supports 'ubuntu', '= 14.04'
+
+recipe 'mumble::server', 'Installs Mumble VoIP server'
