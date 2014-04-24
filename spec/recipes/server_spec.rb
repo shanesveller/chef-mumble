c7b25a75f0063e424b65dce8cd27fb9448ff23c2
Shane Sveller <shanesveller@gmail.com>
2014-04-24 17:17:58 -0500

    Basic test suite

--- spec/recipes/server_spec.rb
+++ spec/recipes/server_spec.rb
@@ -1,7 +1,17 @@
 require_relative '../spec_helper'
 
 describe 'mumble::server' do
-  subject { ChefSpec::Runner.new.converge(described_recipe) }
+  subject do
+    ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') do |node|
+      node.set['mumble']['logfile'] = '/var/log/mumble-server/mumble-server-new.log'
+      node.set['mumble']['pidfile'] = '/var/log/mumble-server/mumble-server-new.pid'
+      node.set['mumble']['port'] = 64_739
+      node.set['mumble']['password'] = '$ERVERP4SSWORD'
+      node.set['mumble']['supassword'] = 'G1MM3R00T'
+      node.set['mumble']['bandwidth'] = 36_000
+      node.set['mumble']['users'] = 50
+    end.converge(described_recipe)
+  end
 
   # Write quick specs using `it` blocks with implied subjects
   it "installs mumble-server" do
@@ -18,5 +28,12 @@ describe 'mumble::server' do
 
   it "starts mumble-server service" do
     expect(subject).to start_service('mumble-server')
+  end
+
+  %w(logfile pidfile port serverpassword bandwidth users).each do |config|
+    it "sets custom config attribute #{config}" do
+      expect(subject).to render_file('/etc/mumble-server.ini').
+        with_content("#{config}=#{subject.node.mumble[config]}")
+    end
   end
 end
