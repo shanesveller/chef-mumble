5b89c9469e27ccd79087cdfd1d970dcc5a894c9c
Shane Sveller <shanesveller@gmail.com>
2014-04-24 17:41:40 -0500

    Use process matching rather than service status

--- test/integration/server/serverspec/mumble_spec.rb
+++ test/integration/server/serverspec/mumble_spec.rb
@@ -5,6 +5,10 @@ include SpecInfra::Helper::DetectOS
 
 describe service('mumble-server') do
   it { should be_enabled }
+end
+
+describe process('murmurd') do
+  its(:args) { should match %r{\-ini /etc/mumble\-server\.ini}}
   it { should be_running }
 end
 
