23e1406dd5a5e394240aad7bb705c107dde12c23
Shane Sveller <shanesveller@gmail.com>
2014-04-24 15:49:53 -0500

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'mumble'
 maintainer       'Shane Sveller'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer_email 'shane@shanesveller.com'
+license          'MIT'
 description      'Installs/Configures mumble'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'apt'
